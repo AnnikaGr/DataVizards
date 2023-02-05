f034cf2141ac415dec173cc82f0b493f383cead0
Annika Grothaus <annika.grothaus@gmx.de>
2023-02-05 22:13:52 +0100

    initial project

---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <p> The data basis for this visualization is the <a href="https://www.europeansocialsurvey.org/" target="_blank" rel="noopener">European Social Survey</a>.</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
