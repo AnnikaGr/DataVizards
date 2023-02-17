3c5a9f7e48d494cda4b51b202c8e01c20beaf95f
Annika Grothaus <annika.grothaus@gmx.de>
2023-02-17 10:58:27 +0100

    refactor

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import MainVisualisation from "../components/PageVisualisation.vue";
-import MainRight from "@/components/Main.vue";
+import Main from "@/components/Main.vue";
 </script>
 
 <template>
 
-    <MainRight />
+    <Main />
 
 </template>
