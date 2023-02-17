ca798e25c840bccf91242ba9b8921693ce4cf96f
Annika Grothaus <annika.grothaus@gmx.de>
2023-02-17 14:30:49 +0100

    visualization data update

--- dataProc.py
+++ dataProc.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sb
 import matplotlib.pyplot as plt
+import csv
 
 df=pd.read_csv("ESS10.csv")
 
@@ -61,6 +62,29 @@ sb.heatmap(posCorr, annot=True, fmt="g", cmap='RdBu_r')
 
 plt.show()
 
-negCorr.to_csv("ESS10-happy-negCorr-bigger-0.2");
-posCorr.to_csv("ESS10-happy-posCorr-bigger-0.2");
+negCorr.columns = ['values']
+posCorr.columns = ['values']
+negCorr.index.name = "labels"
+posCorr.index.name = "labels"
+
+csv_ng= negCorr.to_csv("ESS10-happy-negCorr-bigger-02.csv");
+csv_pos= posCorr.to_csv("ESS10-happy-posCorr-bigger-02.csv");
+
+# csv_values_ng= negCorr.to_csv("ESS10-happy-negCorr-bigger-0.2_values",  columns=['values'], index=False, header=False );
+# csv_labels_ng= negCorr.to_csv("ESS10-happy-negCorr-bigger-0.2_labels", columns=[], header=False);
+#
+# csv_values_pos= posCorr.to_csv("ESS10-happy-posCorr-bigger-0.2_values", columns= ['values'], index=False ,header=False);
+# csv_labels_pos= posCorr.to_csv("ESS10-happy-posCorr-bigger-0.2_labels", columns=[], header=False);
+
+# csv_values_ng_str = ','.join(str(element) for element in csv_values_ng)
+# with open("ESS10-happy-negCorr-bigger-0.2_values.txt", "w") as text_file:
+#     text_file.write(csv_values_ng_str)
+
+
+# csv_file = r'ESS10-happy-negCorr-bigger-0.2_values.csv'
+# txt_file = r'ESS10-happy-negCorr-bigger-0.2_values.txt'
+# with open(txt_file, "w") as my_output_file:
+#     with open(csv_file, "r") as my_input_file:
+#         reader = csv.reader(my_input_file)
+#         my_output_file.write(','.join(row[0] for row in reader))
 
--- src/components/MainVisualisation.vue
+++ src/components/MainVisualisation.vue
@@ -6,49 +6,45 @@
 import * as d3 from "d3";
 import { loadScript } from "vue-plugin-load-script";
 import ApexCharts from "apexcharts";
+import $ from 'jquery'
+import { parse } from '@vanillaes/csv'
 
 loadScript("/src/js/chart1.js");
 
+import { csv } from "d3-fetch";
+    function fetchData() {
+        return csv('/ESS10-happy-posCorr-bigger-02.csv').then((data) => (data));
+    }
+
+
 export default {
   name: "MainVisualisation",
-  mounted() {
+  async mounted() {
+
+    let data = await fetchData();
+    console.log(data)
+
+    let retrieved_values=[];
+    data.forEach(pair => retrieved_values.push(parseFloat(pair.values)))
+    //let retrieved_values_str= retrieved_values.toString()
+
+
+    let retrieved_labels=[];
+    data.forEach(pair => retrieved_labels.push(pair.labels))
+    //let retrieved_labels_str= retrieved_labels.toString()
+
+    console.log(retrieved_values)
+    console.log(retrieved_labels)
+
     var options = {
       series: [
-        0.237, 0.25, 0.268, 0.238, 0.249, 0.232, 0.271, 0.214, 0.205, 0.684,
-        0.305, 0.233, 0.263, 0.221, 0.281, 0.224, 0.227, 0.216, 0.218, 0.221,
-        0.229, 0.297, 0.335,
-      ],
-      labels: [
-        "netusoft",
-        "ppltrst",
-        "pplfair",
-        "pplhlp",
-        "trstprl",
-        "trstlgl",
-        "trstplc",
-        "trstplt",
-        "trstprt",
-        "stflife",
-        "stfeco",
-        "stfgov",
-        "stfdem",
-        "stfedu",
-        "stfhlth",
-        "sclmeet",
-        "inprdsc",
-        "fairelcc",
-        "hscopc19",
-        "vdovexre",
-        "aesfdrk",
-        " health",
-        "hincfel",
+        retrieved_values
       ],
       chart: {
         type: "polarArea",
       },
       colors: [
         function ({ value}) {
-        console.log(value);
           if (value < 0.22) {
             return "#fff7bc";
           } else if (value < 0.3) {
