a48bb564c5e793f3ff3241967884a2108b141d8e
xiaohanp <xiaohan.peng@outlook.com>
2023-02-19 23:15:43 +0100

    change data description

--- src/components/PageAbout.vue
+++ src/components/PageAbout.vue
@@ -20,22 +20,27 @@
         European Social Survey (ESS) in 2022. ESS is a biennial survey of
         Europeans and the chosen dataset contains responses from over 33352
         individuals across 22 European countries. The data was gathered in
-        face-to-face interviews and used questions on a range of topics
-        relating to individual and societal well-being. <br> <br>We selected questions
-        with an absolute correlation stronger than 0.2. While clearing up the
-        data, we deleted the datasets of respondents wherein the individuals did
-        not respond to one of the selected questions as well as overly specific
-        questions. We used Spearman’s rank correlation coefficient to calculate
-        and showcase the strength and direction of the association between
-        various factors and the responses on individuals’ happiness. We used
-        this correlation coefficient since the dataset consisted of ordinal data
-        and Spearman's correlation does not assume a linear relationship between
-        the values.<br><br> It is important to consider that correlations do not imply
-        causation. Further research would be needed to determine whether there
-        are causal relationships between the chosen factors and happiness.
-        Moreover, the data is based on individuals' self-reported answers and
-        might hence be subject to subjectivity and other biases.<br><br>
-        Online resource: <a href="https://ess-search.nsd.no/en/study/172ac431-2a06-41df-9dab-c1fd8f3877e7">https://ess-search.nsd.no/en/study/172ac431-2a06-41df-9dab-c1fd8f3877e7</a>
+        face-to-face interviews and used questions on a range of topics relating
+        to individual and societal well-being. <br />
+        <br />We used Spearman’s rank correlation coefficient to calculate and
+        showcase the strength and direction of the association between various
+        factors and the responses on individuals’ happiness. We used this
+        correlation coefficient since the dataset consisted of ordinal data and
+        Spearman's correlation does not assume a linear relationship between the
+        values. We selected questions with an absolute correlation stronger than
+        0.2. While clearing up the data, we deleted the datasets of respondents
+        wherein the individuals did not respond to one of the selected questions
+        as well as overly specific questions. <br /><br />
+        It is important to consider that correlations do not imply causation.
+        Further research would be needed to determine whether there are causal
+        relationships between the chosen factors and happiness. Moreover, the
+        data is based on individuals' self-reported answers and might hence be
+        subject to subjectivity and other biases.<br /><br />
+        Online resource:
+        <a
+          href="https://ess-search.nsd.no/en/study/172ac431-2a06-41df-9dab-c1fd8f3877e7"
+          >https://ess-search.nsd.no/en/study/172ac431-2a06-41df-9dab-c1fd8f3877e7</a
+        >
       </p>
     </div>
   </div>
